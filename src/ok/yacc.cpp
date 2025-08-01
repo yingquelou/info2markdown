1bcd556d60166c92256f231fc90b8bd88920f566
yingquelou <2695193723@qq.com>
2025-08-02 06:58:07 +0800

    更新词法分析器和语法分析器，添加行解析和字符串替换功能

--- src/ok/yacc.cpp
+++ src/ok/yacc.cpp
@@ -2,21 +2,30 @@
 #define PARSER_HEADER
 #include "pch.h"
 #include <cstdio>
+#include <regex>
 int main(int argc, const char *argv[])
 {
 	void *scanner;
+	FILE *f,*out;
 	for (size_t i = 1; argv[i]; i++)
 	{
-		FILE *f = fopen(argv[i], "rb");
+		f = fopen(argv[i], "rb");
 		if (!f) {
 			fprintf(stderr, "Failed to open file: %s\n", argv[i]);
 			continue;
 		}
+		std::string outname = std::string(argv[i]) + ".md";
+		out = fopen(outname.c_str(), "wb");
+		if (!out) {
+			fprintf(stderr, "Failed to open output file: %s\n", outname.c_str());
+			fclose(f);
+			continue;
+		}
 		yylex_init(&scanner);
+		yyset_out(out, scanner);
 		yyset_in(f, scanner);
-		yy::parser parser(scanner);
 		try {
-			parser();
+			yylex(scanner);
 		} catch (const std::exception& ex) {
 			fprintf(stderr, "Parsing failed for file: %s\nError: %s\n", argv[i], ex.what());
 		}
@@ -25,3 +34,4 @@ int main(int argc, const char *argv[])
 	}
 	return 0;
 }
+// 将字符串中的空格替换为-
